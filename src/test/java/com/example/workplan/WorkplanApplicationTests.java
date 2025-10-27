d857ffbbbc436b6a6f858b7014d01a1263583167
Dennis Cai <60729103+thecodinganalyst@users.noreply.github.com>
2025-10-27 10:13:45 +0800

    Switch to Maven build tooling

---
+++ src/test/java/com/example/workplan/WorkplanApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.workplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
